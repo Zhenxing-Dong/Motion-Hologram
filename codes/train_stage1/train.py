f81807e2c872bb8329efd16e8d5e237d48e18f26
Zhenxing Dong <66579268+Zhenxing-Dong@users.noreply.github.com>
2025-01-02 13:35:48 +0800

    Update train.py

--- codes/train_stage1/train.py
+++ codes/train_stage1/train.py
@@ -39,7 +39,7 @@ def train():
     has_continuous_action_space = False  # continuous action space; else discrete
 
     max_ep_len = 8                  # max timesteps in one episode
-    max_training_timesteps = int(30000)   # break training loop if timeteps > max_training_timesteps
+    max_training_timesteps = int(40000)   # break training loop if timeteps > max_training_timesteps
 
     print_freq = max_ep_len * 10        # print avg reward in the interval (in num timesteps)
     log_freq = max_ep_len * 2           # log avg reward in the interval (in num timesteps)
